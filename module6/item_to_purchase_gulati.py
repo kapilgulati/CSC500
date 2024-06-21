23accde2d7eab04e2b6279154b35863a4f307fb1
kapilgulati <kapil.gulati@outlook.com>
2024-06-20 22:23:09 -0500

    Module 6 - Assignment

--- module6/item_to_purchase_gulati.py
+++ module6/item_to_purchase_gulati.py
@@ -8,38 +8,19 @@ class ItemToPurchase:
 
     # this method will print the item object
     def print_item_cost(self):
+        """
+        Concatenates and returns string consisting of item name, quantity and cost
+        :return: string = formatted value
+        """
         item_cost = self.item_price * self.item_quantity
-        print(f'{self.item_name} {self.item_quantity} @ ${self.item_price} = ${item_cost}')
+        return f'{self.item_name} {self.item_quantity} @ ${self.item_price} = ${item_cost}'
 
-    def has_default_values(self):
-        return self.item_name == "none" and self.item_price == 0.0 and self.item_quantity == 0
-
-
-def main():
-    # define max cart item as variable, this allows us to adjust value from one location.
-    max_cart_items = 2
-    # initialize list for cart item objects
-    cart_items = list(range(max_cart_items))
-    # for loop to get user input, loop will iterate exactly max_cart_items times
-    for i in range(max_cart_items):
-        # print item number
-        print(f"Item {i + 1}")
-        # prompt user to input item details
-        item_name = input("Enter item name: ")
-        item_price = float(input("Enter item price: "))
-        item_quantity = int(input("Enter item quantity: "))
-        # place item in the list
-        cart_items[i] = ItemToPurchase(item_name, item_price, item_quantity)
-    else:
-        # initialize cart total
-        cart_total = 0.0
-        # print cart
-        print("       TOTAL COST")
-        for item in cart_items:
-            item.print_item_cost()
-            cart_total = cart_total + (item.item_quantity * item.item_price)
-        print(f"      Total: ${cart_total}")
-
-
-if __name__ == "__main__":
-    main()
+    def has_default_values(self, dynamic_attr):
+        """
+        Checks and returns True, if dynamic attribute doesn't exist or if the object's class attributes
+        are set to their default values
+        :param dynamic_attr: string - name if the dynamic attribute to check if it exists
+        :return: boolean
+        """
+        return (not hasattr(self, dynamic_attr)) or (
+                    self.item_name.strip().lower() == "none" and self.item_price == 0.0 and self.item_quantity == 0)
--- module6/shopping_cart_gulati.py
+++ module6/shopping_cart_gulati.py
@@ -1,109 +1,192 @@
 """
-Step 4: Build the ShoppingCart class with the following data attributes and related methods. Note: Some can be method stubs (empty methods) initially, to be completed in later steps
-
-Parameterized constructor, which takes the customer name and date as parameters
-Attributes
-customer_name (string) - Initialized in default constructor to "none"
-current_date (string) - Initialized in default constructor to "January 1, 2020"
-cart_items (list)
+Step 4: Build the ShoppingCart class with the following data attributes and related methods.
+Note: Some can be method stubs (empty methods) initially, to be completed in later steps
 
 """
 from item_to_purchase_gulati import ItemToPurchase
 
+# Dynamic attribute for the ItemToPurchase
+ITEM_DESCRIPTION_ATTR = "description"
+
+# Format string global variable
+FORMAT_STRING = "{label:^75}"
+
 
 class ShoppingCart:
+
     def __init__(self, customer_name="none", current_date="January 1, 2020"):
+        """
+        Parameterized default constructor, has the following data attributes:
+        customer_name (string)
+        current_date (string)
+        cart_items (list)
+        :param customer_name: string, Initialized in default constructor to "none"
+        :param current_date: string, Initialized in default constructor to "January 1, 2020"
+
+        """
         self.customer_name = customer_name
         self.current_date = current_date
         self.cart_items = []
 
     def _find_item_by_name(self, item_name):
+        """
+        This private method iterates over the cart items and returns the index of an item for a given item name value.
+        :param item_name: string
+            name value of the item
+        :return: int
+            index of the item matching the given name value, returns "-1" if not found
+        """
         item_to_purchase: ItemToPurchase
         for pos, item_to_purchase in enumerate(self.cart_items):
             if item_to_purchase.item_name == item_name:
                 return pos
         return -1  # indicates not found
 
-    def print_cart_title(self):
-        print(f"{self.customer_name} - {self.current_date}")
+    def get_cart_title(self):
+        """
+        This method returns the concatenated string value of the customer name and current date
+        :return: string = concatenated value
+        """
+        return f"{self.customer_name} - {self.current_date}"
 
-    # Adds an item to cart_items list. Has parameter ItemToPurchase. Does not return anything.
     def add_item(self, item_to_purchase):
+        """
+        Adds an item to the cart_items list.
+        :param item_to_purchase: ItemToPurchase
+            Object of type ItemToPurchase
+        :return: Does not return anything
+        """
         self.cart_items.append(item_to_purchase)
 
-    # Removes item from cart_items list. Has a string (an item's name) parameter. Does not return anything.
-    # If item name cannot be found, output this message: Item not found in cart. Nothing removed.
     def remove_item(self, item_name):
+        """
+        Removes an item from the cart_items list that matches the given item name value. If the item name
+        cannot be found, it outputs this message: "Item not found in cart. Nothing removed."
+        :param item_name: string
+            Item name value
+        :return: Does not return anything.
+        """
         item_index = self._find_item_by_name(item_name)
         if item_index >= 0:
-            item = self.cart_items.pop(self.cart_items.index(item_index))
+            self.cart_items.pop(self.cart_items.index(item_index))
+            # print(f"Item removed from cart: {item_name}")
         else:
             print("Item not found in cart. Nothing removed.")
 
-    # Modifies an item's description, price, and/or quantity. Has parameter ItemToPurchase. Does not return anything.
-    # If item can be found (by name) in cart, check if parameter has default values for
-    # description, price, and quantity. If not, modify item in cart.
-    # If item cannot be found (by name) in cart, output this message: Item not found in cart. Nothing modified.
     def modify_item(self, item_to_purchase: ItemToPurchase):
+        """
+        Modifies an item's description, price, and/ or quantity of cart item that matches the given item name value.
+        If an item can be found (by name) in the cart, it checks if matching item attributes have default values for
+        description, price, and quantity. If not, then modify the item in the cart. If an item cannot be found (by name)
+        in the cart, output this message: "Item not found in the cart. Nothing modified."
+        :param item_to_purchase: ItemToPurchase
+        :return: Does not return anything.
+        """
         item_index = self._find_item_by_name(item_to_purchase.item_name)
         if 0 <= item_index < len(self.cart_items):
-            if not item_to_purchase.has_default_values():
-                # Fixme
-                print(self.cart_items[item_index])
+            if not item_to_purchase.has_default_values(ITEM_DESCRIPTION_ATTR):
                 self.cart_items[item_index] = item_to_purchase
         else:
             print("Item not found in cart. Nothing modified.")
 
-    # Returns quantity of all items in cart. Has no parameters.
     def get_num_items_in_cart(self):
+        """
+        Returns quantity of all items in cart
+        :return: int = number of items in cart
+        """
         cart_quantity = 0
         item_to_purchase: ItemToPurchase
         for item_to_purchase in self.cart_items:
             cart_quantity += item_to_purchase.item_quantity
         return cart_quantity
 
-    # Determines and returns the total cost of items in cart. Has no parameters.
     def get_cost_of_cart(self):
-        cart_total = 0
+        """
+        Determines and returns the total cost of items in the cart. If there are no items in the cart, then returns 0.0
+        :return: float = total cost of items in cart
+        """
+        cart_total = 0.0
         item_to_purchase: ItemToPurchase
         for item_to_purchase in self.cart_items:
             cart_total += item_to_purchase.item_price * item_to_purchase.item_quantity
         return cart_total
 
-    # Outputs total of objects in cart.
-    # If cart is empty, output this message: SHOPPING CART IS EMPTY
     def print_total(self):
+        """
+        Displays Cart title, number of items in the cart, content of the cart and total cost of items in cart
+        If cart is empty, outputs this message: "SHOPPING CART IS EMPTY".
+        :return: Does not return anything.
+        """
         if len(self.cart_items) > 0:
-            self.print_cart_title()
-            print(f"Number of Items: {self.get_num_items_in_cart()}")
+            print(FORMAT_STRING.format(label="OUTPUT SHOPPING CART"))
+            print(FORMAT_STRING.format(label=self.get_cart_title()))
+            print(FORMAT_STRING.format(label=f"Number of Items: {self.get_num_items_in_cart()}"))
             c_item: ItemToPurchase
             for c_item in self.cart_items:
-                c_item.print_item_cost()
-            print(f"Total: ${self.get_cost_of_cart()}")
-
+                print(FORMAT_STRING.format(label=c_item.print_item_cost()))
+            print(FORMAT_STRING.format(label=f"Total: ${self.get_cost_of_cart()}"))
         else:
             print("SHOPPING CART IS EMPTY")
 
-    # print_descriptions()
-    # Outputs each item's description.
     def print_descriptions(self):
+        """
+        Outputs/displays cart title and each item's description in the cart
+        :return: Does not return anything
+        """
+        print(FORMAT_STRING.format(label="OUTPUT ITEMS' DESCRIPTIONS"))
+        print(FORMAT_STRING.format(label=self.get_cart_title()))
+        print(FORMAT_STRING.format(label="Item Descriptions"))
         cart_item: ItemToPurchase
-        self.print_cart_title()
-        print("Item Descriptions")
         for cart_item in self.cart_items:
-            print(f"{cart_item.item_name}: {cart_item.__getattribute__('description')}")
+            print(FORMAT_STRING.format(label=f"{cart_item.item_name}: {getattr(cart_item, ITEM_DESCRIPTION_ATTR)}"))
 
 
-if __name__ == "__main__":
+def print_menu():
+    print(FORMAT_STRING.format(label="MENU"))
+    print(FORMAT_STRING.format(label="a - Add item to cart"))
+    print(FORMAT_STRING.format(label="r - Remove item from cart"))
+    print(FORMAT_STRING.format(label="c - Change item quantity"))
+    print(FORMAT_STRING.format(label="i - Output items' descriptions"))
+    print(FORMAT_STRING.format(label="o - Output shopping cart"))
+    print(FORMAT_STRING.format(label="q - Quit"))
+
+
+def main():
+    # prime the cart with sample data
     shopping_cart = ShoppingCart("John Doe's Shopping Cart", "February 1, 2020")
     item = ItemToPurchase("Nike Romaleos", 189, 2)
-    item.__setattr__("description", "Volt color, Weightlifting shoes")
+    setattr(item, "description", "Volt color, Weightlifting shoes")
     shopping_cart.add_item(item)
     item = ItemToPurchase("Chocolate Chips", 3, 5)
-    item.__setattr__("description", "Semi-sweet")
+    setattr(item, "description", "Semi-sweet")
     shopping_cart.add_item(item)
     item = ItemToPurchase("Powerbeats 2 Headphones", 128, 1)
-    item.__setattr__("description", "Bluetooth headphones")
+    setattr(item, "description", "Bluetooth headphones")
     shopping_cart.add_item(item)
-    shopping_cart.print_total()
-    shopping_cart.print_descriptions()
+    # infinite loop, print menu and ask user to select a menu option
+    while True:
+        print_menu()
+        selected_menu = input("Choose an option:")
+        if selected_menu == 'q':
+            # quit loop
+            break
+        elif selected_menu == "i":
+            # output cart description
+            shopping_cart.print_descriptions()
+            print("\n")
+        elif selected_menu == "o":
+            # output cart totals
+            shopping_cart.print_total()
+            print("\n")
+        elif selected_menu in ['a', 'r', 'c']:
+            print(FORMAT_STRING.format(label="Menu option not supported try q, i or o option"))
+            print("\n")
+        else:
+            print(FORMAT_STRING.format(label="Enter a valid menu option"))
+            print("\n")
+
+
+# Tests class implementation
+if __name__ == "__main__":
+    main()
+
